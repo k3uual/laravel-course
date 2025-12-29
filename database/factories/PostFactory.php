3e5b6f28005e9171ea1be119bf369b15b7fddce0
k3uual <kewaltailor345@gmail.com>
2025-12-29 14:14:31 +0530

    Added posts on frontend

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,14 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->sentence();
         return [
-            //
+            'image'=>fake()->imageUrl(),
+            'title'=> $title,
+            'slug'=> \Illuminate\Support\Str::slug($title),
+            'content'=> fake()->paragraph(5),
+            'category_id'=> Category::inRandomOrder()->first()->id,
+            'user_id'=> 1,
         ];
     }
 }
